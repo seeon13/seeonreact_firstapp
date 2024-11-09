8c60ccbd6531a8b60059844565f09abd405e2ec6
zion <zion13@handong.ac.kr>
2024-11-09 17:46:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="description">
+          {/* 추가한 설명 섹션 */}
+          <p>Seeon's first react app</p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
